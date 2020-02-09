41084ee8535c18305ba7bc142a4a6d1c686252bd
weweaaa <a0926422280@gmail.com>
2020-02-09 11:52:00 +0800

    調整：元件切割

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SocialIconsComponent } from './header/social-icons/social-icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SocialIconsComponent
   ],
   imports: [
     BrowserModule,
